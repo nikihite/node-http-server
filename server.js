a2d0a0ebce96f4ff3a0070b34cd131a887998321
Niki Hite <nikihite@comcast.net>
2022-09-16 14:55:42 -0700

    all 4, 404 tests passing

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import http from 'node:http';
-import app from './lib/app.js';
+import http from "node:http";
+import app from "./lib/app.js";
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
 
-server.listen(PORT, () => {
+server.listen(PORT, HOST, () => {
     console.log(server.address());
 });
